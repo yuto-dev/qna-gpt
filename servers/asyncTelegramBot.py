b2d4499c2ade3f849acacdcd6eac5ec0df58ea73
Abiyyu Bagindo <directx178@gmail.com>
2024-04-22 14:40:37 +0700

    Removed redundant callGPT function from Telegram Bot

--- servers/asyncTelegramBot.py
+++ servers/asyncTelegramBot.py
@@ -13,63 +13,6 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-def callGPT(prompt):
-    # Define the URL
-    url = "http://localhost:8001/v1/completions"
-    # Define the headers
-    headers = {
-        "Content-Type": "application/json"
-    }
-
-    # Define the data
-    data = {
-        "prompt": prompt,
-        "stream": False,
-        "use_context": True,
-        "system_prompt": "Kamu adalah LibraryAI. Kamu adalah AI LLM yang memiliki akses ke database literatur fisik. Gunakan literatur ini untuk menjawab pertanyaan yang diajukan padamu. Jika kamu tidak dapat menemukan jawabannya dalam informasi tersebut, katakan saja bahwa kamu tidak yakin dan berikan perkiraan tentang apa yang kamu pikirkan sebagai jawabannya, pastikan untuk memberi tahu pengguna bahwa ini adalah tebakan terbaikmu dan tidak 100 persen benar."
-    }
-
-    # Make the POST request
-    response = requests.post(url, headers=headers, data=json.dumps(data))
-    
-    # Check if the request was successful
-    if response.status_code ==  200:
-        # Parse the JSON response
-        response_data = response.json()
-        jsonContent = response_data.keys()
-        # Extract choices key
-        choices = response_data.get('choices')
-        #--------------------------------------------
-        #Extract content
-        message = choices[0].get('message')
-        content = message.get('content')
-        print(content)
-        #--------------------------------------------
-        sources = choices[0].get('sources')
-    
-        i = 0
-        sourcesList = []
-        for entry in sources:
-            document = sources[i].get('document')
-            doc_metadata = document.get('doc_metadata')
-            page_label = doc_metadata.get('page_label')
-            file_name = doc_metadata.get('file_name')
-        
-            number = str(i + 1)
-            formattedSource = number + ". " + file_name + " (page " + page_label + ")"
-            sourcesList.append(formattedSource)
-            i = i + 1
-        
-        i = 0
-        for source in sources:
-            print(sourcesList[i])
-            i = i + 1  
-    
-    else:
-        print("Request failed with status code:", response.status_code)
-    
-    return content, sourcesList
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(update.effective_chat.id)
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
